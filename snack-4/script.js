df7f03aaed3bad6c6470335554545992ed7c57a5
Francesco Cassese <francescocassese9@gmail.com>
2026-04-14 15:35:28 +0200

    feat: completato snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map(({ author }) => {
+  return author;
+});
+
+console.log(authors);
